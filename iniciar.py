f52f28d240a20f080b29f67659107d2ae8e28b1a
Yeison Acero <yeijan@gmail.com>
2026-03-25 17:06:48 -0500

    Suma de dos numeros

--- iniciar.py
+++ iniciar.py
@@ -1,1 +1,5 @@
-print("Hello, World!");
+print("Suma de dos números")
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
