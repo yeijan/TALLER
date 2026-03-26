6e551d9f73f1d1f09fa8f45ecb6acd7eeb541763
Yeison Acero <yeijan@gmail.com>
2026-03-25 20:54:53 -0500

    Se actualiza el codigo para la suma de dos numero

--- iniciar.py
+++ iniciar.py
@@ -1,1 +1,6 @@
-print("Hola Mundos")
+print("Bienvenido al programa de inicio")
+print("Esta aplicacion suma dos numeros")
+numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("Ingrese el segundo numero: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es: ", resultado)
